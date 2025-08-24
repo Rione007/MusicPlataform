082a15fd2a870757862f71f9fd2f5aa545d4566e
David Peña <d1806pena@gmail.com>
2025-08-24 15:16:37 -0500

    Login, index, layout

--- MusicPlataform.Client/Controllers/UsersController.cs
+++ MusicPlataform.Client/Controllers/UsersController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using MusicPlataform.Client.Models;
 
 namespace MusicPlataform.Client.Controllers
@@ -15,6 +19,7 @@ namespace MusicPlataform.Client.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
+
 
         // GET: Users/Register
         public IActionResult Register() => View();
@@ -50,7 +55,11 @@ namespace MusicPlataform.Client.Controllers
         }
 
         // GET: Users/Login
-        public IActionResult Login() => View();
+        public IActionResult Login()
+        {
+            ViewData["Layout"] = "_Layout2";
+            return View();
+        }
 
         // POST: Users/Login
         [HttpPost]
@@ -60,12 +69,11 @@ namespace MusicPlataform.Client.Controllers
 
             var client = _httpClientFactory.CreateClient("MusicApi");
 
-            // DTO que espera el API
             var dto = new
             {
                 Username = model.Username,
-                Email = string.Empty, // no se usa en login
-                Password = model.Password // en texto plano, el API lo verifica
+                Email = string.Empty,
+                Password = model.Password
             };
 
             var content = new StringContent(
@@ -76,10 +84,37 @@ namespace MusicPlataform.Client.Controllers
             var response = await client.PostAsync("users/login", content);
 
             if (response.IsSuccessStatusCode)
-                return RedirectToAction("Index", "Home");
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, model.Username),
+                };
 
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync
+                (
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal,
+                    new AuthenticationProperties
+                    {
+                        IsPersistent = true, 
+                        ExpiresUtc = DateTime.UtcNow.AddHours(1)
+                    }
+                );
+
+                return RedirectToAction("Main", "Home");
+            }
             ModelState.AddModelError("", "Usuario o contraseña incorrectos");
             return View(model);
         }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
